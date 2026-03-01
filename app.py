8912cbb0d4b772a72c0805b2364d25870efb6d5d
root <root@ip-172-31-3-138.ec2.internal>
2026-03-01 12:22:38 +0000

    mypythoncode

---
+++ app.py
@@ -0,0 +1,9 @@
+n1 = float(input("Enter first number: "))
+n2 = float(input("Enter second number: "))
+print("Addition:", n1+n2)
+print("Subtraction:", n1-n2)
+print("Multiplication:", n1*n2)
+if n2 != 0:
+    print("Division:", n1/n2)
+else:
+    print("Division: Cannot divide by zero")
